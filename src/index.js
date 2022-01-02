80ad1d3da832e4526f832b31efcfca34907f572d
WiiJoy MacOS <wiijoy@bk.ru>
2022-01-02 18:26:16 +0300

    feat: add function's body

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 module.exports = function reverse (n) {
-  
+  let numToStr = '' + Math.sqrt(Math.pow(n, 2));
+  let reverseStr = numToStr.split('').reverse().join('');
+
+  return Number(reverseStr)
 }
